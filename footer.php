75f3ad189e33179911a43aa184221223c812e1ca
subash tharu <subashtharu116@gmail.com>
2019-10-27 21:36:43 -0700

    footer file added

---
+++ footer.php
@@ -0,0 +1,37 @@
+
+<!-- footer -->
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h4>Contact Us</h4>
+				 <ul class="contact">
+					<li><i class="fa fa-map-marker"></i> &nbsp;<strong>Address:</strong> &nbsp; Magadi Main Road, East West College, Anjananagar, Bangalore, 560091</li>
+					<li><i class="fa fa-phone"></i> &nbsp;<strong>Phone:</strong> &nbsp;9123456789</li>
+					<li><i class="fa fa-envelope-o"></i> &nbsp;<strong>Email:</strong> &nbsp; info@eatlunchbox.in</li>
+				</ul>
+			</div>
+			<div class="col-md-6">
+				<h4>Follow on Facebook</h4>
+				 <div class="fb-page" data-href="https://www.facebook.com/eatlunchbox2019" data-tabs="timeline" data-width="" data-height="100" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/eatlunchbox2019" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/eatlunchbox2019">Eatlunchbox</a></blockquote></div>
+			</div>
+		</div>
+	</div>
+	<!-- End of footer -->
+	<!-- Bottom footer -->
+	  <nav class="navbar navbar-inverse no-margin">
+		<div class="container">
+			<div class="footer-bottom">
+				<center><h6 style="color: red;font-size: 25px;margin:0;">eatlunchbox.in</h6> 
+				<em style="color: #9c5e5e;font-size: 14px;">Online lunch ordering service</em>
+				<p>Copyright &copy eatlunchbox.in All Rgiht Reserved</p></center>
+			</div>
+		</div>
+		<div class="creator" style="float:right;">
+				<p id="creator-details" style="margin:0;color: #555555;font-weight: 500;">Creator : Subash Tharu </p>
+				<em style="color: #3e3c39;font-size: 14px;">subashtharu116@gmail.com</em>
+
+			</div>
+	</nav>
+	<!-- End of bottom fotter -->
+</footer>
